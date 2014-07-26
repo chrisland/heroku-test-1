6e44bca5c09f479e90c9d6f80e0ef30b267ef3f3
christian marienfeld <post@chrisland.de>
2014-07-26 15:58:48 +0200

    add db

--- web.js
+++ web.js
@@ -5,10 +5,27 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World 1!');
+  res.send('Hello World with DB !!');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOSOUP_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
