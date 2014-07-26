96b3ab15eb971ee3caa4b867f2dc21453b89e3f7
christian marienfeld <post@chrisland.de>
2014-07-26 16:28:03 +0200

    test db

--- web.js
+++ web.js
@@ -8,6 +8,21 @@ app.get('/', function(req, res) {
   res.send('Hello World with DB !!');
 });
 
+app.get('/db', function(req, res) {
+	res.send('DB !! WUHU !!!!');
+	
+	var mongo = require('mongodb');
+	
+	
+	var mongoUri = process.env.MONGOSOUP_URI ||
+		process.env.MONGOSOUP_URL ||
+		'mongodb://localhost/mydb';
+	
+	res.send(' # '+mongoUri);
+	
+	
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
@@ -16,12 +31,11 @@ app.listen(port, function() {
 
 
 
-var mongo = require('mongodb');
-/*
 
-var mongoUri = process.env.MONGOSOUP_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
+
+
+
+/*
 
 mongo.Db.connect(mongoUri, function (err, db) {
   db.collection('mydocs', function(er, collection) {
@@ -29,5 +43,6 @@ mongo.Db.connect(mongoUri, function (err, db) {
     });
   });
 });
+
 
 */
